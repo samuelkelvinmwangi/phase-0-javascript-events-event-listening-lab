d39f364bd93f286da074975b2a114d64475b095d
samuel <samuelkelvinmwangi@gmail.com>
2023-04-16 19:32:58 +0300

    assingnment finishup

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
-}
+    const input = document.getElementById('main');
+    input.addEventListener('Input', (event) => {
+      console.log('Input changed:', event.target.value);
+    });
+  }
